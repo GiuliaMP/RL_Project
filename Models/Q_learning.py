4748f01b232f00e07f0b4058236de0e74d21af3e
Giulia Marchiori Pietrosanti <48836873+GiuliaMP@users.noreply.github.com>
2022-07-08 11:52:22 +0200

    Small adjustment on Sarsa_va_ANN and reved dead code from Q_learning and Sarsa.

--- Models/Q_learning.py
+++ Models/Q_learning.py
@@ -4,15 +4,13 @@ import collections
 def q_learning(env, discretization, ep_min_decay, alpha, gamma, episodes, render=False):
     actions = [action for action in range(env.action_space.n)]
     tot_reward = []
-    #q_table = init_Qtable(state_number, action_number)
     q_table = collections.defaultdict(float)
     for ep in range(episodes):
         eps = decay_function(ep, ep_min_decay)
         tot_ep_reward = 0
         done = False
-        s = env.reset() # seed = 42
+        s = env.reset()
         s = discretize_state(s, discretization)
-        i = 0
         while not done:
             a = choose_action_eps_greedy(env, q_table, s, eps)
             s_p, reward, done, _ = env.step(a)
@@ -23,7 +21,6 @@ def q_learning(env, discretization, ep_min_decay, alpha, gamma, episodes, render
             q_table[s,a] += alpha*(reward + gamma*(np.max(q_action_s_p)) - q_table[s,a])
             s = s_p
             tot_ep_reward += reward
-            i+=1
         tot_reward.append(tot_ep_reward)
     env.close()
     return tot_reward
--- Models/Sarsa.py
+++ Models/Sarsa.py
@@ -3,7 +3,6 @@ import collections
 
 def sarsa(env, discretization, ep_min_decay, alpha, gamma, episodes, render=False):
     tot_reward = []
-    #q_table = init_Qtable(state_number, action_number)
     q_table = collections.defaultdict(float)
     eps_list = []
     for ep in range(episodes):
@@ -11,10 +10,9 @@ def sarsa(env, discretization, ep_min_decay, alpha, gamma, episodes, render=Fals
         eps_list.append(eps)
         tot_ep_reward = 0
         done = False
-        s = env.reset() # seed = 42
+        s = env.reset()
         s = discretize_state(s, discretization)
         a = choose_action_eps_greedy(env, q_table, s, eps)
-        i = 0
         while not done:
             s_p, reward, done, _ = env.step(a)
             if render:
@@ -24,7 +22,6 @@ def sarsa(env, discretization, ep_min_decay, alpha, gamma, episodes, render=Fals
             q_table[s,a] += alpha*(reward + gamma*q_table[s_p,a_p] - q_table[s,a])
             s, a = s_p, a_p
             tot_ep_reward += reward
-            i+=1
         tot_reward.append(tot_ep_reward)
     env.close()
     plt.plot(eps_list)
