43610c4486e7c01ac80c202e38a681a537d44297
valebl <valebl@DESKTOP-06R2CFC.localdomain>
2022-07-16 09:24:13 +0200

    Small fix in q-learning ann.

--- Models/Q_learning_ANN.py
+++ Models/Q_learning_ANN.py
@@ -7,12 +7,8 @@ import numpy as np
 from collections import deque
 
 device = "cpu" # torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
-UPDATE_EVERY = 4        # how often to update the network
 
 class Net(nn.Module):
     def __init__(self, input_size, output_size, seed):
